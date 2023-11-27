eb62b84e3d5a8b5835c23b1f137b077da7997adc
Daniil Palii <daniil.palii@proton.me>
2023-11-27 21:14:01 +0100

    Add MAUI project

---
+++ DataPeersync.Client.Maui/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace DataPeersync.Client.Maui;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ DataPeersync.Client.Maui/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace DataPeersync.Client.Maui;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
