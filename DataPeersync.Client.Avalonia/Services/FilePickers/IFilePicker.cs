90bf95662b4e022898fe9c842bb356d1126ee356
Daniil Palii <daniil.palii@proton.me>
2024-01-13 19:03:21 +0100

    Remove Avalonia projects

--- DataPeersync.Client.Avalonia/Services/FilePickers/IFilePicker.cs
+++
@@ -1,9 +0,0 @@
-using System.Threading.Tasks;
-
-namespace DataPeersync.Client.Avalonia.Services.FilePickers
-{
-	public interface IFilePicker
-	{
-		Task<string?> PickAsync();
-	}
-}
--- DataPeersync.Client.Avalonia/Services/ServiceProvider/IServiceProvider.cs
+++
@@ -1,9 +0,0 @@
-using DataPeersync.Client.Avalonia.Services.FilePickers;
-
-namespace DataPeersync.Client.Avalonia.Services.ServiceProvider
-{
-	public interface IServiceProvider
-	{
-		IFilePicker FilePicker { get; }
-	}
-}
